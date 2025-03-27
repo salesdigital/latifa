d7bdd0fa387bcdce2a38c40da6325f89e6190843
salesdigital <164252854+salesdigital@users.noreply.github.com>
2025-03-26 23:37:23 -0300

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -30,11 +31,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W2ZPRR84');
+        `}
+      </Script>
+      
+      {/* Google tag (gtag.js) */}
+      <Script 
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16937601495" 
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16937601495');
+        `}
+      </Script>
+      
       <body
         data-qb-extension-installed="true"
         data-qb-installed="true"
         className={`${geistMono.variable} ${signora.variable} ${tanpearl.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2ZPRR84"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
